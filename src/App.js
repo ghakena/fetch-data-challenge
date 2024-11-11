e04e6cf2a8ac52c403ee6dd60d14951ce2c2f4a3
Gibbs Herbert Akena <akenagibbsherbert6@gmail.com>
2024-11-11 15:30:25 +0300

    created table component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import Form from './Form';
 import List from './List';
+import Table from './Table';
 
 function App() {
   const API_URL = 'https://jsonplaceholder.typicode.com/';
@@ -33,7 +34,10 @@ function App() {
           reqType={reqType}
           setReqType={setReqType}
         />
-        <List 
+        {/* <List 
+          items={items}
+        /> */}
+        <Table 
           items={items}
         />
     </div>
